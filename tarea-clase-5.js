878b159e7992204f3f53bd59b97addbe53e6e27c
davidlp90 <davidivan.eda@gmail.com>
2021-09-08 02:29:40 -0300

    Deleted lines
    Deletenes code lines

--- tarea-clase-5.js
+++ tarea-clase-5.js
@@ -1,9 +1,3 @@
-//TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
-//cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
-// en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
-
-
-
 document.querySelector('#ingresar').onclick = function() {
     const $inputAnnualSalary = Number(document.querySelector('#ingresar-salario').value) || '';
     const $monthlySalary = calculateMonthlySalary($inputAnnualSalary)
@@ -13,9 +7,6 @@ document.querySelector('#ingresar').onclick = function() {
     } else {
         noSalaryInput = 'Esa plata nos pertenece a todes!'
     }
-
-
-
     document.querySelector('#salario-mensual').value = $monthlySalary;
     document.querySelector('#resultado').innerText = noSalaryInput;
     return false;
